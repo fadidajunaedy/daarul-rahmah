4d59f513269097317b284c4f796db855f46878d0
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2024-01-05 12:37:40 +0700

    CLEAN SOME LINE

--- resources/views/admin/about/edit-mision.blade.php
+++ resources/views/admin/about/edit-mision.blade.php
@@ -12,11 +12,11 @@
     </div>
     <h2 class="font-semibold my-4 mb-4">Edit Misi</h2>
     @include('components.alert')
-    <div class="overflow-x-auto bg-base-100 p-4 rounded-xl shadow-xl">
+    <div class="grid grid-cols-3 gap-4">
         <form 
         action="{{ url('admin/about/mision/' . $data->id . '/update') }}" 
         method="POST"
-        class=" grid grid-cols-2 gap-4"
+        class="col-span-2 flex flex-col gap-4 bg-base-100 p-4 rounded-lg shadow-xl"
         >
             @csrf
             @method('PATCH')
@@ -33,9 +33,20 @@
                 <textarea name="description" class="textarea textarea-lg textarea-bordered">{{ $data->description }}</textarea>
             </div>
             <div class="col-span-2 flex justify-end gap-2">
+                <a href="{{ url()->previous() }}" class="btn btn-lg">Cancel</a>
                 <button type="submit" class="btn btn-lg btn-cta btn-primary">Save Changes</button>
             </div>
         </form>
+        <div class="col-span-3 lg:col-span-1 bg-base-100 p-6 rounded-xl shadow-xl flex flex-col gap-2 h-[280px]">
+            <div>
+                <span class="font-semibold">Created At</span>
+                <p>{{ $data->created_at->diffForHumans() }}</p>
+            </div>
+            <div>
+                <span class="font-semibold">Updated At</span>
+                <p>{{ $data->updated_at->diffForHumans() }}</p>
+            </div>
+        </div>
     </div>
 </section>
 @endsection
